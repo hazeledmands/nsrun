7eb181def75e0ff00ba190f52e9540d56f311866
Max Edmands <git@maxedmands.com>
2016-06-04 09:45:22 -0700

    Add better tests

--- test.js
+++ test.js
@@ -1,1 +1,43 @@
-it('works');
+const nixt = require('nixt');
+
+it('runs the scripts you define in package.json', function (done) {
+  nixt()
+  .run('./nsrun.js tell-me-about-monkeys')
+  .stdout('Monkeys are fun')
+  .stderr('')
+  .code(0)
+  .end(done);
+});
+
+it('runs nested scripts with nsrun instead of npm run', function (done) {
+  nixt()
+  .run('./nsrun.js tell-me-more-about-monkeys')
+  .stdout('Monkeys are fun\nBut they can be a lot of work too.')
+  .stderr('')
+  .code(0)
+  .end(done);
+});
+
+it('exits with the same code as the script', function (done) {
+  nixt()
+  .run('./nsrun.js fail')
+  .stdout('')
+  .stderr('')
+  .code(1)
+  .end(done);
+});
+
+it('outputs all the scripts in package.json with no arguments', function (done) {
+  nixt()
+  .run('./nsrun.js')
+  .stdout([
+    '4 scripts in ./package.json:',
+    ' * test',
+    ' * tell-me-about-monkeys',
+    ' * tell-me-more-about-monkeys',
+    ' * fail'
+  ].join('\n'))
+  .stderr('')
+  .code(0)
+  .end(done);
+});
