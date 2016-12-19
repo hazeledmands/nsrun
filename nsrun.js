bfbce8f94afd78dd6c45d26aca3ed39d27535b2e
Max Edmands <git@maxedmands.com>
2016-12-19 14:52:44 -0800

    Remove kexec dependency

--- nsrun.js
+++ nsrun.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 "use strict"
 
+const cp = require('child_process');
 const find = require('lodash.find');
 const fs = require('fs');
-const kexec = require('kexec');
 const minimist = require('minimist');
 const path = require('path');
 
@@ -40,13 +40,14 @@ if (scripts.length === 0) {
 }
 
 /** ************** FIGURE OUT WHAT SCRIPT TO RUN *****************/
+const currentProcessPath = process.argv.slice(0, 2).join(' ');
 const argv = minimist(process.argv.slice(2));
 const args = argv._ || [];
 const scriptName = args.shift();
 const scriptToRun = find(scripts, {name: scriptName});
 
 /** ************* LIST ALL SCRIPTS ON NO INPUT *******************/
-if (!scriptToRun) {
+if (!scriptName) {
   console.log(`${scripts.length} scripts in ${humanReadablePkgPath}:`);
   scripts.forEach(function (script) {
     console.log(` * ${script.name}`);
@@ -54,22 +55,43 @@ if (!scriptToRun) {
   process.exit(0);
 }
 
-/** ************* RUN SCRIPT *************************************/
-if (scriptToRun) {
-  process.env.PATH = path.join(pkgPath, 'node_modules', '.bin') + ':' + process.env.PATH;
-
-  // convert npm run-script (and all aliases) to nsrun
-  scriptToRun.script = scriptToRun.script
-    .replace(/npm run-script/g, 'nsrun')
-    .replace(/npm run/g, 'nsrun')
-    .replace(/npm test/g, 'nsrun test')
-    .replace(/npm start/g, 'nsrun start')
-    .replace(/npm stop/g, 'nsrun stop')
-    .replace(/npm restart/g, 'nsrun restart');
-
-  kexec(scriptToRun.script + ' ' + args.map(function (arg) { return `'${arg}'` }).join(' '));
+/** ************* ERROR ******************************************/
+if (!scriptToRun) {
+  console.log(`${humanReadablePkgPath}: No script found with name "${scriptName}"`);
+  process.exit(1);
 }
 
-/** ************* ERROR ******************************************/
-console.log(`${humanReadablePkgPath}: No script found with name "${scriptName}"`);
-process.exit(1);
+/** ************* RUN SCRIPT *************************************/
+process.env.PATH = path.join(pkgPath, 'node_modules', '.bin') + ':' + process.env.PATH;
+
+// convert npm run-script (and all aliases) to nsrun
+scriptToRun.script = scriptToRun.script
+  .replace(/npm run-script/g, currentProcessPath)
+  .replace(/npm run/g, currentProcessPath)
+  .replace(/npm test/g, currentProcessPath + ' test')
+  .replace(/npm start/g, currentProcessPath + ' start')
+  .replace(/npm stop/g, currentProcessPath + ' stop')
+  .replace(/npm restart/g, currentProcessPath + ' restart');
+
+const command = scriptToRun.script + ' ' +
+  args.map(function (arg) { return `'${arg}'` }).join(' ');
+
+const child = cp.exec(command);
+child.stdout.pipe(process.stdout);
+child.stderr.pipe(process.stderr);
+child.on('error', function (err) { console.error(err.stack); });
+child.on('exit', function (code, signal) { process.exit(code); });
+
+[
+  'SIGUSR1',
+  'SIGTERM',
+  'SIGINT',
+  'SIGPIPE',
+  'GITHUP',
+  'SIGBREAK',
+  'SIGWINCH'
+].map(function (signal) {
+  process.on(signal, function () {
+    child.kill(signal);
+  });
+});
