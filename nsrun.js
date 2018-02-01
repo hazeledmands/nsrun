71d1c12ccb54631e2ddb307fcb5a8d3c75a1a1d8
Max Edmands <max@goodeggs.com>
2018-02-01 08:35:13 -0800

    Add prettier, use prettier

--- nsrun.js
+++ nsrun.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-"use strict"
+"use strict";
 
-const cp = require('child_process');
-const find = require('lodash.find');
-const fs = require('fs');
-const minimist = require('minimist');
-const path = require('path');
+const cp = require("child_process");
+const find = require("lodash.find");
+const fs = require("fs");
+const minimist = require("minimist");
+const path = require("path");
 
 let pkgPath = process.cwd();
 let pkgJsonFile = null;
@@ -14,24 +14,26 @@ let pkgJson = null;
 /** ***************** FIND PACKAGE.JSON ******************/
 while (pkgJson === null && path.dirname(pkgPath) !== pkgPath) {
   try {
-    pkgJsonFile = path.join(pkgPath, 'package.json');
+    pkgJsonFile = path.join(pkgPath, "package.json");
     pkgJson = require(pkgJsonFile);
   } catch (e) {
     pkgPath = path.dirname(pkgPath);
   }
 }
 if (pkgJson === null) {
-  console.log('Could not find a package.json in the current directory or any parent directories.');
-  console.log('Are you in a node project?');
+  console.log(
+    "Could not find a package.json in the current directory or any parent directories."
+  );
+  console.log("Are you in a node project?");
   process.exit(1);
 }
 const humanReadablePkgPath = `./${path.relative(process.cwd(), pkgJsonFile)}`;
 
 /** **************** PARSE OUT SCRIPTS FROM PACKAGE.JSON ***********/
-const scripts = Object.keys(pkgJson.scripts || {}).map(function (name) {
+const scripts = Object.keys(pkgJson.scripts || {}).map(function(name) {
   return {
     name,
-    script: pkgJson.scripts[name],
+    script: pkgJson.scripts[name]
   };
 });
 if (scripts.length === 0) {
@@ -40,88 +42,95 @@ if (scripts.length === 0) {
 }
 
 /** ************** FIGURE OUT WHAT SCRIPT TO RUN *****************/
-const currentProcessPath = process.argv.slice(0, 2).join(' ');
+const currentProcessPath = process.argv.slice(0, 2).join(" ");
 const argv = minimist(process.argv.slice(2));
-let   args = argv._ || [];
+let args = argv._ || [];
 const scriptName = args.shift();
-const scriptToRun = find(scripts, {name: scriptName});
+const scriptToRun = find(scripts, { name: scriptName });
 
 /** ************* LIST ALL SCRIPTS ON NO INPUT *******************/
 if (!scriptName) {
   console.log(`${scripts.length} scripts in ${humanReadablePkgPath}:`);
-  scripts.forEach(function (script) {
+  scripts.forEach(function(script) {
     console.log(` * ${script.name}`);
-  })
+  });
   process.exit(0);
 }
 
 /** ************* ERROR ******************************************/
 if (!scriptToRun) {
-  console.log(`${humanReadablePkgPath}: No script found with name "${scriptName}"`);
+  console.log(
+    `${humanReadablePkgPath}: No script found with name "${scriptName}"`
+  );
   process.exit(1);
 }
 
 /** ************* RUN SCRIPT *************************************/
-process.env.PATH = path.join(pkgPath, 'node_modules', '.bin') + ':' + process.env.PATH;
+process.env.PATH =
+  path.join(pkgPath, "node_modules", ".bin") + ":" + process.env.PATH;
 
 // convert npm run-script (and all aliases) to nsrun
 scriptToRun.script = scriptToRun.script
   .replace(/npm run-script/g, currentProcessPath)
   .replace(/npm run/g, currentProcessPath)
-  .replace(/npm test/g, currentProcessPath + ' test')
-  .replace(/npm start/g, currentProcessPath + ' start')
-  .replace(/npm stop/g, currentProcessPath + ' stop')
-  .replace(/npm restart/g, currentProcessPath + ' restart');
+  .replace(/npm test/g, currentProcessPath + " test")
+  .replace(/npm start/g, currentProcessPath + " start")
+  .replace(/npm stop/g, currentProcessPath + " stop")
+  .replace(/npm restart/g, currentProcessPath + " restart");
 
-args = args.map(function (arg) { return `'${arg}'` });
-let command = '';
-  
-if ( (/\$[0-9]|\$\*/).test(scriptToRun.script) )
-{
+args = args.map(function(arg) {
+  return `'${arg}'`;
+});
+let command = "";
+
+if (/\$[0-9]|\$\*/.test(scriptToRun.script)) {
   let found = [];
 
-  command = scriptToRun.script.replace(/(\$[0-9])/g,function(v){
-    v=v.substr(1) - 1;
-    if ( typeof args[v] === 'undefined' ) return '$'+(v++);
+  command = scriptToRun.script.replace(/(\$[0-9])/g, function(v) {
+    v = v.substr(1) - 1;
+    if (typeof args[v] === "undefined") return "$" + v++;
     found.push(v);
     return args[v];
   });
 
   // TODO : Add a --nsrun-no-clear to preserve arguments
-  found.map(function(v){args[v]=''});
+  found.map(function(v) {
+    args[v] = "";
+  });
 
-  command = command.replace(/(\$\*)/,args.join(' '));
+  command = command.replace(/(\$\*)/, args.join(" "));
 
-  if ( (/\$[0-9]/).test(command) )
-  {
-    console.log('Arguments in script missmatchs');
+  if (/\$[0-9]/.test(command)) {
+    console.log("Arguments in script missmatchs");
     process.exit(1);
   }
-}
-else
-{
-  command = scriptToRun.script + ' ' + args.join(' ');
+} else {
+  command = scriptToRun.script + " " + args.join(" ");
 }
 
 const child = cp.spawn(command, {
   cwd: process.cwd(),
   env: process.env,
-  stdio: 'inherit',
+  stdio: "inherit",
   shell: true
 });
-child.on('error', function (err) { console.error(err.stack); });
-child.on('exit', function (code, signal) { process.exit(code); });
+child.on("error", function(err) {
+  console.error(err.stack);
+});
+child.on("exit", function(code, signal) {
+  process.exit(code);
+});
 
 [
-  'SIGUSR1',
-  'SIGTERM',
-  'SIGINT',
-  'SIGPIPE',
-  'GITHUP',
-  'SIGBREAK',
-  'SIGWINCH'
-].map(function (signal) {
-  process.on(signal, function () {
+  "SIGUSR1",
+  "SIGTERM",
+  "SIGINT",
+  "SIGPIPE",
+  "GITHUP",
+  "SIGBREAK",
+  "SIGWINCH"
+].map(function(signal) {
+  process.on(signal, function() {
     child.kill(signal);
   });
 });
